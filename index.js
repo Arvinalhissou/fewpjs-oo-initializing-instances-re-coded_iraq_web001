8375e2f92569617b5d8401bfa6fb212a5ee87996
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-06-12 21:04:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Write your code here
 class Breakfast {
-  constructor(food,drink)
+  constructor(food,drink){}
 }
 
 class Lunch{
-  constructor(salad,soup,drink)
+  constructor(salad,soup,drink){}
+}
+
+class Dinner{
+  constructor()
 }
